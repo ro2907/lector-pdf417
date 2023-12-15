f88842ed290d6bfdac6df68d208537a2ba4e8c9b
ro2907 <powerjujuy1@gmail.com>
2023-12-15 10:07:24 -0300

    segunda prueba

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
 Quagga.init({
     inputStream: {
-      name: "Live",
-      type: "LiveStream",
-      target: document.querySelector('#your-scanner-element'), // Agrega un elemento HTML para mostrar la vista previa de la cámara
+        name: "Live",
+        type: "LiveStream",
+        target: document.querySelector('#your-scanner-element'),
     },
     decoder: {
-      readers: ["pdf417_reader"], // Utiliza el lector de PDF417
+        readers: ["pdf417_reader"],
     },
-  }, function(err) {
+}, function(err) {
     if (err) {
-      console.error(err);
-      return;
+        console.error(err);
+        document.getElementById('error').textContent = 'Error al iniciar el escáner: ' + err;
+        return;
     }
     Quagga.start();
-  });
-  
-  Quagga.onDetected(function(result) {
-    console.log(result.codeResult.code); // Aquí puedes procesar los datos del código PDF417
-  });
-  
+});
+
+Quagga.onDetected(function(result) {
+    const code = result.codeResult.code;
+    document.getElementById('result').textContent = 'Resultado: ' + code;
+});
+
+Quagga.onProcessed(function(result) {
+    const drawingCtx = Quagga.canvas.ctx.overlay;
+    const drawingCanvas = Quagga.canvas.dom.overlay;
+
+    if (result) {
+        if (result.boxes) {
+            drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+            result.boxes.filter(function (box) {
+                return box !== result.box;
+            }).forEach(function (box) {
+                Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "green", lineWidth: 2 });
+            });
+        }
+
+        if (result.box) {
+            Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#00F", lineWidth: 2 });
+        }
+
+        if (result.codeResult && result.codeResult.code) {
+            Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
+        }
+    }
+});
